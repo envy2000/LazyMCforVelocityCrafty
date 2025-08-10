a02058892e1ef29117060accc6a74b69bab691f9
envy2000 <63881241+envy2000@users.noreply.github.com>
2025-08-10 01:01:25 -0500

    Update PluginConfig.java

--- src/main/java/com/lazymcvelocitycrafty/config/PluginConfig.java
+++ src/main/java/com/lazymcvelocitycrafty/config/PluginConfig.java
@@ -1,1 +1,19 @@
+package com.lazymcvelocitycrafty.config;
 
+import java.util.Map;
+
+public class PluginConfig {
+  private String craftyBaseUrl;
+  private String craftyApiKey;
+  private String lobbyServer;
+  private long maxStartWaitSeconds;
+  private long checkIntervalSeconds;
+  private Map<String, String> serverUuidMap;
+
+  public String getCraftyBaseUrl() { return craftyBaseUrl; }
+  public String getCraftyApiKey() { return craftyApiKey; }
+  public String getLobbyServer() { return lobbyServer; }
+  public long getMaxStartWaitSeconds() { return maxStartWaitSeconds; }
+  public long getCheckIntervalSeconds() { return checkIntervalSeconds; }
+  public Map<String, String> getServerUuidMap() { return serverUuidMap; }
+}

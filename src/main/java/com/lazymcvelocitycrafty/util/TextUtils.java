30ec12b597e9e0f20acdfd4a016d947db433974b
envy2000 <63881241+envy2000@users.noreply.github.com>
2025-08-10 02:26:10 -0500

    Update TextUtils.java

--- src/main/java/com/lazymcvelocitycrafty/util/TextUtils.java
+++ src/main/java/com/lazymcvelocitycrafty/util/TextUtils.java
@@ -1,1 +1,9 @@
+package com.lazymcvelocitycrafty.util;
 
+import net.kyori.adventure.text.Component;
+
+public class TextUtils {
+  public static Component text(String message) {
+    return Component.text(message);
+  }
+}
